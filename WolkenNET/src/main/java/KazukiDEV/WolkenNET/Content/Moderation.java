698e91af2d3f66bd3c6f0d4be53587b5bc60dfdd
KazukiDEV <herpersmarcandre3@gmail.com>
2021-01-15 15:09:22 +0100

    Mobile Klicks anzeigen und Mobile Views

--- WolkenNET/src/main/java/KazukiDEV/WolkenNET/Content/Moderation.java
+++ WolkenNET/src/main/java/KazukiDEV/WolkenNET/Content/Moderation.java
@@ -6,8 +6,8 @@ public class Moderation {
 
 	public static void BanUser(String username) {
 		u.s.println(u.warning + "The User " + Color.RED + username + Color.RESET + " was banned");
-		String ban_sql = "UPDATE `users` SET `banned`= true WHERE `username` = ?";
-		mysql.Exec(ban_sql, username);
+		String ban_sql = "UPDATE `users` SET `banned`= true, `session` = ? WHERE `username` = ?";
+		mysql.Exec(ban_sql, Auth.generateSessionCookie(), username);
 	}
 	
 }
